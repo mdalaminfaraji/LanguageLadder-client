045a80a787f023aba960076b85934009a22dee70
mdalaminfaraji <alaminice1617@gmail.com>
2023-06-14 17:40:25 +0600

    Create AreaChart

---
+++ src/Pages/Dahsboard/Instructor/AreaChart.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { AreaChart, Area, XAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+const AreaChart = () => {
+    const data = [
+        { name: 'Jan', value: 400 },
+        { name: 'Feb', value: 300 },
+        { name: 'Mar', value: 600 },
+        { name: 'Apr', value: 800 },
+        { name: 'May', value: 500 },
+        { name: 'Jun', value: 900 },
+      ];
+    return (
+            <ResponsiveContainer width="100%" height={300}>
+            <AreaChart data={data}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <Tooltip />
+                <Area type="monotone" dataKey="value" fill="#8884d8" />
+            </AreaChart>
+            </ResponsiveContainer>
+    );
+};
+
+export default AreaChart;
