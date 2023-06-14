56a28ff474e3829c1335476c048c91f6b5276645
mdalaminfaraji <alaminice1617@gmail.com>
2023-06-14 18:02:58 +0600

    rechart instructur home page done

--- src/Pages/Dahsboard/Instructor/InstructorChart.jsx
+++ src/Pages/Dahsboard/Instructor/InstructorChart.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import { AreaChart, Area, XAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import useInstructorClasses from '../../../Hooks/useInstructorClasses';
 const InstructorChart = () => {
+    const [refetch, classes]=useInstructorClasses();
+    const TotalPrice=classes.reduce((sum, item)=>sum+item.price, 0);
+    const AvailableSeats=classes.reduce((sum, item)=>sum+item.availableSeats, 0);
+    const classesLength=classes.length;
     const data = [
-        { name: 'Jan', value: 400 },
-        { name: 'Feb', value: 300 },
-        { name: 'Mar', value: 600 },
-        { name: 'Apr', value: 800 },
-        { name: 'May', value: 500 },
-        { name: 'Jun', value: 900 },
+        { name: 'Instructor Classes', value: classesLength},
+        { name: 'Total Price', value: TotalPrice },
+        { name: 'availableSeats', value: AvailableSeats },
+       
       ];
+      console.log(classes);
     return (
             <ResponsiveContainer width="100%" height={300}>
             <AreaChart data={data}>
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="name" />
                 <Tooltip />
+                
                 <Area type="monotone" dataKey="value" fill="#8884d8" />
             </AreaChart>
             </ResponsiveContainer>
